7188a7d14e1b4fd1e5dcabc2cc58bb771be2acd8
Minhaz <minhazul.khan@selise.ch>
2023-03-21 22:02:35 +0600

    added fields

--- wordpress2/wp-content/plugins/contact-plugin/includes/options_page.php
+++ wordpress2/wp-content/plugins/contact-plugin/includes/options_page.php
@@ -6,14 +6,24 @@ use Carbon_Fields\Field;
 add_action('after_setup_theme', 'load_carbon_fields');
 add_action('carbon_fields_register_fields', 'create_options_page');
 
-function load_carbon_fields(){
+function load_carbon_fields()
+{
     \Carbon_Fields\Carbon_Fields::boot();
 }
 
-function create_options_page(){
-    Container::make( 'theme_options', __( 'Contact Options' ) )
-    ->add_fields( array(
-        Field::make( 'text', 'crb_facebook_url', __( 'Facebook URL' ) ),
-        Field::make( 'textarea', 'crb_footer_text', __( 'Footer Text' ) )
-    ) );
+function create_options_page()
+{
+    $plugin_prefix = "contact_plugin_";
+    Container::make('theme_options', __('Contact Form'))
+        ->add_fields(array(
+            Field::make("checkbox", $plugin_prefix . "active", __("Active")),
+
+            Field::make('text', $plugin_prefix . "recipients", __('Recipient Email'))
+                ->set_attribute("placeholder", "eg: your@mail.com")
+                ->set_help_text("The email that the form is submitted to"),
+
+            Field::make('textarea', $plugin_prefix . "message", __('Confirmation Message'))
+                ->set_attribute("placeholder", "Enter confirmation message")
+                ->set_help_text("Type the message you want the submitter to receive"),
+        ));
 }
