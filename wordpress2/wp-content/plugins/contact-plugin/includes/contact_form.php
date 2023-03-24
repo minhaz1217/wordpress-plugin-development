ef3cc5260ea4731de14cdcfeb0d16ed328de2fa0
Minhaz <minhaz1217@gmail.com>
2023-03-24 14:33:13 +0600

    form showing and receiving data in the BE

--- wordpress2/wp-content/plugins/contact-plugin/includes/contact_form.php
+++ wordpress2/wp-content/plugins/contact-plugin/includes/contact_form.php
@@ -1,7 +1,32 @@
 <?php
 
 add_shortcode("contact", "show_contact_form");
+
+add_action("rest_api_init", "create_rest_endpoint");
+
 function show_contact_form()
 {
-    return "our form should load";
+    include MY_PLUGIN_PATH . '/includes/templates/contact-form.php';
+}
+
+function create_rest_endpoint()
+{
+    register_rest_route("v1/contact-form", "submit", array(
+        "methods" => "POST",
+        "callback" => "handle_enquiry"
+    ));
+}
+
+function handle_enquiry($data)
+{
+    $params = $data->get_params();
+    if (!wp_verify_nonce($params["_wpnonce"], "wp_rest")) {
+        return new WP_REST_Response("Message not sent", 422);
+    }
+
+    unset($params["_wpnonce"]);
+    unset($params["_wp_http_referer"]);
+
+
+    return new WP_REST_Response("Message sent", 200);
 }
---
+++ wordpress2/wp-content/plugins/contact-plugin/includes/templates/contact-form.php
@@ -0,0 +1,35 @@
+<form id='enquiry_form'>
+    <?php wp_nonce_field('wp_rest') ?>
+    <label>Name</label> <br />
+    <input type="text" name="name" /><br />
+
+    <label>Email</label> <br />
+    <input type="text" name="email" /><br />
+
+    <label>Phone</label> <br />
+    <input type="text" name="phone" /><br />
+
+    <label>Your Message</label> <br />
+    <textarea name="message"></textarea><br />
+
+    <button type="submit">Submit Form</button>
+</form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script>
+    jQuery(document).ready(
+        function($) {
+            $("#enquiry_form").submit(function(event) {
+                event.preventDefault();
+
+                var form = $(this);
+
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo get_rest_url(null, 'v1/contact-form/submit') ?>",
+                    data: form.serialize()
+                });
+            });
+
+        }
+    );
+</script>
